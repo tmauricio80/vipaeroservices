42059e395e4c3f4fb5e8bdb1fcd453bbd6098aa8
Mauricio <tmauricio80@gmail.com>
2021-06-17 21:40:06 -0400

    Last commit

--- js/main.js
+++ js/main.js
@@ -1,137 +1,193 @@
 (function ($) {
- "use strict";
+    "use strict";
 
-/*----------------------------
- jQuery MeanMenu
------------------------------- */
-	jQuery('nav#dropdown').meanmenu();	
-	
-/*----------------------------
- wow js active
------------------------------- */
- new WOW().init();
- 
-/*----------------------------
- owl active
------------------------------- */  
-  $(".total-team").owlCarousel({
-      autoPlay: true, 
-	  slideSpeed:2000,
-	  pagination:true,
-	  navigation:false,	  
-      items : 4,
-	  /* transitionStyle : "fade", */    /* [This code for animation ] */
-	  navigationText:["<i class='fa fa-angle-left'></i>","<i class='fa fa-angle-right'></i>"],
-      itemsDesktop : [1199,4],
-	  itemsDesktopSmall : [980,3],
-	  itemsTablet: [768,1],
-	  itemsMobile : [479,1],
-  });
- 
-/*----------------------------
-Home Testimonial
------------------------------- */  
-  $(".home-testimonial").owlCarousel({
-      autoPlay: true, 
-	  slideSpeed:2000,
-	  pagination:true,
-	  navigation:false,	  
-      items : 1,
-	  /* transitionStyle : "fade", */    /* [This code for animation ] */
-	  navigationText:["<i class='fa fa-angle-left'></i>","<i class='fa fa-angle-right'></i>"],
-      itemsDesktop : [1199,1],
-	  itemsDesktopSmall : [980,1],
-	  itemsTablet: [768,1],
-	  itemsMobile : [479,1],
-  });
+    /*----------------------------
+     jQuery MeanMenu
+    ------------------------------ */
+    jQuery('nav#dropdown').meanmenu();
 
- /*----------------------------
- Sidebar Testimonial
- ------------------------------ */  
-   $(".sidebar-testimonial").owlCarousel({
-       autoPlay: true, 
- 	  slideSpeed:2000,
- 	  pagination:true,
- 	  navigation:false,	  
-       items : 1,
- 	  /* transitionStyle : "fade", */    /* [This code for animation ] */
- 	  navigationText:["<i class='fa fa-angle-left'></i>","<i class='fa fa-angle-right'></i>"],
-       itemsDesktop : [1199,1],
- 	  itemsDesktopSmall : [980,1],
- 	  itemsTablet: [768,1],
- 	  itemsMobile : [479,1],
-   });
+    /*----------------------------
+     wow js active
+    ------------------------------ */
+    new WOW().init();
 
- /*----------------------------
- Single Project Image Slider
- ------------------------------ */  
-   $(".single-project-slider").owlCarousel({
-       autoPlay: true, 
- 	  slideSpeed:2000,
- 	  pagination:false,
- 	  navigation:true,	  
-       items : 1,
- 	  /* transitionStyle : "fade", */    /* [This code for animation ] */
- 	  navigationText:["<i class='fa fa-angle-left'></i>","<i class='fa fa-angle-right'></i>"],
-       itemsDesktop : [1199,1],
- 	  itemsDesktopSmall : [980,1],
- 	  itemsTablet: [768,1],
- 	  itemsMobile : [479,1],
-   });
+    /*----------------------------
+     owl active
+    ------------------------------ */
+    $(".total-team").owlCarousel({
+        autoPlay: true,
+        slideSpeed: 2000,
+        pagination: true,
+        navigation: false,
+        items: 4,
+        /* transitionStyle : "fade", */    /* [This code for animation ] */
+        navigationText: ["<i class='fa fa-angle-left'></i>", "<i class='fa fa-angle-right'></i>"],
+        itemsDesktop: [1199, 4],
+        itemsDesktopSmall: [980, 3],
+        itemsTablet: [768, 1],
+        itemsMobile: [479, 1],
+    });
 
-/*----------------------------
- price-slider active
------------------------------- */  
-	  $( "#slider-range" ).slider({
-	   range: true,
-	   min: 40,
-	   max: 600,
-	   values: [ 60, 570 ],
-	   slide: function( event, ui ) {
-		$( "#amount" ).val( "£" + ui.values[ 0 ] + " - £" + ui.values[ 1 ] );
-	   }
-	  });
-	  $( "#amount" ).val( "£" + $( "#slider-range" ).slider( "values", 0 ) +
-	   " - £" + $( "#slider-range" ).slider( "values", 1 ) );  
-	   
-/*--------------------------
- scrollUp
----------------------------- */	
-	$.scrollUp({
+    /*----------------------------
+    Home Testimonial
+    ------------------------------ */
+    $(".home-testimonial").owlCarousel({
+        autoPlay: true,
+        slideSpeed: 2000,
+        pagination: true,
+        navigation: false,
+        items: 1,
+        /* transitionStyle : "fade", */    /* [This code for animation ] */
+        navigationText: ["<i class='fa fa-angle-left'></i>", "<i class='fa fa-angle-right'></i>"],
+        itemsDesktop: [1199, 1],
+        itemsDesktopSmall: [980, 1],
+        itemsTablet: [768, 1],
+        itemsMobile: [479, 1],
+    });
+
+    /*----------------------------
+    Sidebar Testimonial
+    ------------------------------ */
+    $(".sidebar-testimonial").owlCarousel({
+        autoPlay: true,
+        slideSpeed: 2000,
+        pagination: true,
+        navigation: false,
+        items: 1,
+        /* transitionStyle : "fade", */    /* [This code for animation ] */
+        navigationText: ["<i class='fa fa-angle-left'></i>", "<i class='fa fa-angle-right'></i>"],
+        itemsDesktop: [1199, 1],
+        itemsDesktopSmall: [980, 1],
+        itemsTablet: [768, 1],
+        itemsMobile: [479, 1],
+    });
+
+    /*----------------------------
+    Single Project Image Slider
+    ------------------------------ */
+    $(".single-project-slider").owlCarousel({
+        autoPlay: true,
+        slideSpeed: 2000,
+        pagination: false,
+        navigation: true,
+        items: 1,
+        /* transitionStyle : "fade", */    /* [This code for animation ] */
+        navigationText: ["<i class='fa fa-angle-left'></i>", "<i class='fa fa-angle-right'></i>"],
+        itemsDesktop: [1199, 1],
+        itemsDesktopSmall: [980, 1],
+        itemsTablet: [768, 1],
+        itemsMobile: [479, 1],
+    });
+
+    /*----------------------------
+     price-slider active
+    ------------------------------ */
+    $("#slider-range").slider({
+        range: true,
+        min: 40,
+        max: 600,
+        values: [60, 570],
+        slide: function (event, ui) {
+            $("#amount").val("ï¿½" + ui.values[0] + " - ï¿½" + ui.values[1]);
+        }
+    });
+    $("#amount").val("ï¿½" + $("#slider-range").slider("values", 0) +
+        " - ï¿½" + $("#slider-range").slider("values", 1));
+
+    /*--------------------------
+     scrollUp
+    ---------------------------- */
+    $.scrollUp({
         scrollText: '<i class="fa fa-angle-up"></i>',
         easingType: 'linear',
         scrollSpeed: 900,
         animation: 'fade'
     });
-/*-------------------------------
-Counter Up
----------------------------------*/     
-	$('.about-counter').counterUp({
-	delay: 50,
-	time: 3000
-	}); 
+    /*-------------------------------
+    Counter Up
+    ---------------------------------*/
+    $('.about-counter').counterUp({
+        delay: 50,
+        time: 3000
+    });
 
-/*-------------------------------------
-Jquery 	Color pannel setting
--------------------------------------*/
-$('span.skype-color').on('click',function(){
-	$('body').addClass('skype-color-body').removeClass('green-color-body red-color-body blue-color-body');
-});
-$('span.red-color').on('click',function(){
-	$('body').addClass('red-color-body').removeClass('green-color-body skype-color-body blue-color-body');
-});
-$('span.green-color').on('click',function(){
-	$('body').addClass('green-color-body').removeClass('red-color-body skype-color-body blue-color-body');
-});
-$('span.blue-color').on('click',function(){
-	$('body').addClass('blue-color-body').removeClass('red-color-body green-color-body skype-color-body');
-});
-$('.cross-button').on('click',function(){
-	$('.demo-panel-setting-area').toggleClass( "highlight" );
-});
+    /*-------------------------------------
+    Jquery 	Color pannel setting
+    -------------------------------------*/
+    $('span.skype-color').on('click', function () {
+        $('body').addClass('skype-color-body').removeClass('green-color-body red-color-body blue-color-body');
+    });
+    $('span.red-color').on('click', function () {
+        $('body').addClass('red-color-body').removeClass('green-color-body skype-color-body blue-color-body');
+    });
+    $('span.green-color').on('click', function () {
+        $('body').addClass('green-color-body').removeClass('red-color-body skype-color-body blue-color-body');
+    });
+    $('span.blue-color').on('click', function () {
+        $('body').addClass('blue-color-body').removeClass('red-color-body green-color-body skype-color-body');
+    });
+    $('.cross-button').on('click', function () {
+        $('.demo-panel-setting-area').toggleClass("highlight");
+    });
+
+})(jQuery);
+
+function submitForm() {
+    let values = jQuery('#consultation-form').serializeArray();
+    let errorMessage = '';
+    let process = true;
+    $.each(values, function (index, value) {
+        if (value.name === 'company' && value.value === '') {
+            errorMessage += 'Company Field Required<br />';
+            process = false;
+        }
+
+        if (value.name === 'full-name' && value.value === '') {
+            errorMessage += 'Full Name Field Required<br />';
+            process = false;
+        }
+
+        if (value.name === 'email' && value.value === '') {
+            errorMessage += ' Email Field Required<br />';
+            process = false;
+        }
+
+        if (value.name === 'phone' && value.value === '') {
+            errorMessage += 'Phone Field Required<br />';
+            process = false;
+        }
+
+    });
+
+    if (!process) {
+        $.alert({
+            title: 'Alert!',
+            content: errorMessage,
+        });
+        return;
+    }
 
 
 
-  
- 
-})(jQuery); 
+    $.post("./submit.php", function (data) {
+        console.log(`data`);
+        console.log(data);
+        // $(".result").html(data);
+    });
+
+}
+
+
+function modifyFormFields(e) {
+    $('.optional-field').hide();
+    let placeholder = 'Add your details here';
+    let selectedOption = $('#sel1').val();
+    if (selectedOption === 'quote-product') {
+        $('.optional-field').show();
+    } else if (selectedOption === 'quote-aeroplane') {
+        placeholder = 'Plane Details';
+    } else if (selectedOption === 'quote-service') {
+        placeholder = 'Maintenance Details';
+    }
+    $('#details').attr("placeholder", placeholder);
+}
--- submit.php
+++ submit.php
@@ -1,3 +1,47 @@
 <?php
+//Import PHPMailer classes into the global namespace
+//These must be at the top of your script, not inside a function
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
 
-echo 12;
+//Load Composer's autoloader
+require 'vendor/autoload.php';
+
+//Instantiation and passing `true` enables exceptions
+$mail = new PHPMailer(true);
+
+try {
+    //Server settings
+    $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+    $mail->isSMTP();                                            //Send using SMTP
+    $mail->Host       = 'secure.emailsrvr.com';                     //Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+    $mail->Username   = 'info@vipaeroservices.com';                     //SMTP username
+    $mail->Password   = 'Info2021';                               //SMTP password
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         //Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
+    $mail->Port       = 465;                                    //TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
+
+    //Recipients
+    $mail->setFrom('info@vipaeroservices.com', 'VIP Aeroservices');
+    $mail->addAddress('tmauricio80@gmail.com', 'Mauricio Tovar');     //Add a recipient
+//     $mail->addAddress('ellen@example.com');               //Name is optional
+//     $mail->addReplyTo('info@example.com', 'Information');
+//     $mail->addCC('cc@example.com');
+//     $mail->addBCC('bcc@example.com');
+
+    //Attachments
+//     $mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
+//     $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
+
+    //Content
+    $mail->isHTML(true);                                  //Set email format to HTML
+    $mail->Subject = 'Here is the subject';
+    $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+    $mail->send();
+    echo 'Message has been sent';
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+}
